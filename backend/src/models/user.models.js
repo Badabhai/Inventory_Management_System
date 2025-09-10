dc45d36ab49c2cab864c61f183eb5ef9a402e2f8
Suraj Yadav <codegenerator999@gmail.com>
2025-09-10 23:49:00 +0530

    added middleware & methods in userModel

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema(
     {
@@ -22,5 +24,48 @@ const userSchema = new mongoose.Schema(
     },
     {timestamps: true}
 )
+
+userSchema.pre("save", async function (next) {
+    //If password is not modified then just return
+    if(!this.isModified("password")) return next()
+
+    //If password is modified then encrypt it
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+//Method to verify Password
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+//Method to Generate Access Token
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            userName: this.userName,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+//Method to Generate Refresh Token
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 
 export const User = mongoose.model("User",userSchema)
